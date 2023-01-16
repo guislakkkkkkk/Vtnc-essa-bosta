b6802906fc423b1b288764311731c0e68d074697
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2023-01-16 10:27:16 -0300

    acho q deu

--- assets/preload/stages/vecindad.lua
+++ assets/preload/stages/vecindad.lua
@@ -1,16 +1,16 @@
 function onCreatePost()
 	
-	makeLuaSprite('bg1', 'chavobg/Vecindad', -600, 200);
+	makeLuaSprite('bg1', 'chavo/images/chavobg/Vecindad', -600, 200);
 	addLuaSprite('bg1', false);
-	makeAnimatedLuaSprite('back', 'chavobg/Quico_Background', -700, 200)
+	makeAnimatedLuaSprite('back', 'chavo/images/chavobg/Quico_Background', -700, 200)
 	addAnimationByPrefix('back', 'bounce','Quico_Background Idle', 15, true);
 	addLuaSprite('back', false);
-	makeAnimatedLuaSprite('gfb', 'chavobg/GF', 600, 800)
+	makeAnimatedLuaSprite('gfb', 'chavo/images/chavobg/GF', 600, 800)
 	addAnimationByPrefix('gfb', 'bounce','GF Idle', 15, true);
 	addLuaSprite('gfb', false);
 
 
-	makeLuaSprite('bg2', 'chavobg/VecindadPOV', -600, 200);
+	makeLuaSprite('bg2', 'chavo/images/chavobg/VecindadPOV', -600, 200);
 	addLuaSprite('bg2', false);
 	
 	setProperty('bg2.visible', false);
